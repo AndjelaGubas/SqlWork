660df19468f2e0d3ceba9b7f9269aec957ed8d53
Andjela Gubas <andjela.marti@gmail.com>
2025-03-08 10:51:22 +0100

    found salary of specific employee

---
+++ salary_of_employees.sql
@@ -0,0 +1,17 @@
+delimiter //
+create procedure salary_of_employees(
+	IN emp_no INT)
+begin
+	select 
+		emp.emp_no, emp.first_name, emp.last_name, sal.amount, de.from_date, de.to_date
+	from 
+		employee emp
+		left join salary sal on 
+        emp.emp_no = sal.emp_no 
+        left join dept_emp de on
+        emp.emp_no = de.emp_no
+	where 
+		emp_no = p_emp_no and 
+        to_date = '9999-01-01';
+end //
+delimiter;

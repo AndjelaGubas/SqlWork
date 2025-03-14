9081777d4987e35e3e727acf72e502c1d6e6e7e1
Andjela Gubas <andjela.marti@gmail.com>
2025-03-14 15:25:51 +0100

    changing status of department managers

---
+++ add_dept_manager.sql
@@ -0,0 +1,36 @@
+drop procedure if exists add_dept_manager;
+delimiter //
+create procedure add_dept_manager(
+	IN p_emp_no INT,
+    IN p_dept_no CHAR(4)
+    )
+proc: begin
+DECLARE l_is_exists INT DEFAULT 0;
+DECLARE l_m_is_exists INT DEFAULT 0;
+
+
+	select count(*) into l_is_exists from dept_emp where emp_no = p_emp_no and dept_no = p_dept_no;
+		if l_is_exists > 0 then
+			select 'zaposleni je vec u departmentu';
+			leave proc;
+		end if;
+
+	select count(*) into l_m_is_exists from dept_manager where dept_no = p_dept_no;
+		if l_m_is_exists > 0 then 
+			select 'zaposleni je vec menadzer u departmanu';
+			leave proc;
+		end if;
+    
+	update dept_manager
+		set to_date = now()
+		where dept_no =p_dept_no and
+		to_date = '9999-01-01';
+    
+	insert into dept_manager (emp_no, dept_no, from_date, to_date) values (p_emp_no, p_dept_no, now(), '9999-01-01');
+
+	insert into audit (date_action, dept_no, emp_no, audit_action, `description`)
+		values (now(), p_dept_no, p_emp_no, 'addman', concat('dodat novi menadzer' , p_emp_no, ' ' ,p_dept_no, 'at' ,now()));
+
+end //
+delimiter ;
+
